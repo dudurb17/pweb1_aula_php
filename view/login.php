3c0d0a2c4b80208592a646497bc7cff7b5c402e1
dudurb17 <eduardo.robettibedin@gmail.com>
2023-06-28 22:02:49 -0300

    metade do login

--- view/login.php
+++ view/login.php
@@ -15,17 +15,45 @@ if (!empty($_POST)) {
 }
 $dados = !empty($_SESSION['dados']) ? $_SESSION['dados'] : "";
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
 
-<h3>Sistema Academico</h3>
-<form action="login.php" method="post">
-    <p style="color:red;">
-        <?php echo (!empty($_SESSION["msg"]) ? $_SESSION["msg"] : "") ?>
-    </p>
-    <label>Login</label>
-    <input type="text" name="login" value="<?php echo (!empty($dados['login']) ? $dados['login'] : "") ?>" /><br>
-    <label>Senha</label>
-    <input type="password" name="senha" /><br>
-    <button type="submit">Logar</button>
-    <button><a href="RegistrarUsuarioForm.php">Registrar-se</a></button>
-</form>
+<div class="container">
+    <h1>Sistema do Pet Feliz</h1>
+    <form action="login.php" method="post">
+        <div>
+            <div class="card align-items-center " style='width:400px '>
+                <div class="row ">
+                    <div class="col">
+                        <div class="card-body align-items-center">
+                            <p style="color:red;">
+                                <?php echo (!empty($_SESSION["msg"]) ? $_SESSION["msg"] : "") ?>
+                            </p>
+                            <div class="container text-center">
+                                <input type="text" name="login" class="form-control" placeholder="Informe o usuario"
+                                    value="<?php echo (!empty($dados['login']) ? $dados['login'] : "") ?>" /><br>
+                            </div>
+                            <div class="container text-center">
+                                <input type="password" name="senha" class="form-control"
+                                    placeholder="Informe a senha" /><br>
+                            </div>
+
+
+                            <button type="submit" class='btn btn-success'>Logar</button>
+                            <button class='btn btn-warning'><a href="RegistrarUsuarioForm.php">Registrar-se</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+    </form>
+</div>
 <?php include "./base/rodape.php" ?>
