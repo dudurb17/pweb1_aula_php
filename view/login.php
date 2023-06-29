0145161033e766d8cb3eefb2ea6b11b578e7ae84
dudurb17 <eduardo.robettibedin@gmail.com>
2023-06-29 09:56:54 -0300

    centralizando o login

--- view/login.php
+++ view/login.php
@@ -19,34 +19,38 @@ $dados = !empty($_SESSION['dados']) ? $_SESSION['dados'] : "";
     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
 
 <div class="container">
-    <h1>Sistema do Pet Feliz</h1>
-    <form action="login.php" method="post">
-        <div>
-            <div class="card align-items-center " style='width:400px '>
-                <div class="row ">
-                    <div class="col">
-                        <div class="card-body align-items-center">
-                            <p style="color:red;">
-                                <?php echo (!empty($_SESSION["msg"]) ? $_SESSION["msg"] : "") ?>
-                            </p>
-                            <div class="container text-center">
-                                <input type="text" name="login" class="form-control" placeholder="Informe o usuario"
-                                    value="<?php echo (!empty($dados['login']) ? $dados['login'] : "") ?>" /><br>
-                            </div>
-                            <div class="container text-center">
-                                <input type="password" name="senha" class="form-control"
-                                    placeholder="Informe a senha" /><br>
-                            </div>
+    <div>
+
+        <h1 class="text-center">Sistema do Pet Feliz</h1></br>
+        <form action="login.php" method="post">
+            <div class="d-flex justify-content-center">
+
+                <div class="card align-items-center " style=' width:400px '>
+                    <div class="row ">
+                        <div class="col">
+                            <div class="card-body align-items-center">
+                                <p style="color:red;">
+                                    <?php echo (!empty($_SESSION["msg"]) ? $_SESSION["msg"] : "") ?>
+                                </p>
+                                <div class="container text-center">
+                                    <input type="text" name="login" class="form-control" placeholder="Informe o usuario"
+                                        value="<?php echo (!empty($dados['login']) ? $dados['login'] : "") ?>" /><br>
+                                </div>
+                                <div class="container text-center">
+                                    <input type="password" name="senha" class="form-control"
+                                        placeholder="Informe a senha" /><br>
+                                </div>
 
 
-                            <button type="submit" class='btn btn-success'>Logar</button>
-                            <button class='btn btn-warning'><a href="RegistrarUsuarioForm.php">Registrar-se</a></button>
+                                <button type="submit" class=' btn btn-success'>Logar</button>
+                                <button class='btn btn-warning'><a
+                                        href="RegistrarUsuarioForm.php">Registrar-se</a></button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-
+    </div>
 
 
 
