9c1c455c3fe8455ecac8a9e550cf263bdbf68401
dudurb17 <eduardo.robettibedin@gmail.com>
2023-06-30 11:35:24 -0300

    banco de dados

--- sql/db_aula.sql
+++ sql/db_aula.sql
@@ -19,6 +19,17 @@
 CREATE DATABASE IF NOT EXISTS `db_aula` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `db_aula`;
 
+-- Copiando estrutura para tabela db_aula.estoque
+CREATE TABLE IF NOT EXISTS `estoque` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
+  `quantidade` int NOT NULL DEFAULT '0',
+  `preco` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Exportação de dados foi desmarcado.
+
 -- Copiando estrutura para tabela db_aula.pedido
 CREATE TABLE IF NOT EXISTS `pedido` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -26,13 +37,9 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   `quantidade` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
   `valor` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Copiando dados para a tabela db_aula.pedido: ~2 rows (aproximadamente)
-INSERT INTO `pedido` (`id`, `nome`, `quantidade`, `valor`) VALUES
-	(1, 'osso', '20', '10'),
-	(7, 'tico', '23', '34'),
-	(8, 'teste', '45', '546');
+-- Exportação de dados foi desmarcado.
 
 -- Copiando estrutura para tabela db_aula.pet
 CREATE TABLE IF NOT EXISTS `pet` (
@@ -42,12 +49,9 @@ CREATE TABLE IF NOT EXISTS `pet` (
   `idade` int NOT NULL DEFAULT '0',
   `porte` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Copiando dados para a tabela db_aula.pet: ~0 rows (aproximadamente)
-INSERT INTO `pet` (`id`, `nome`, `raca`, `idade`, `porte`) VALUES
-	(1, 'cabrito', 'peludo', 10, 'med'),
-	(4, 'Cachorro', 'pintcher', 5, 'pequeno');
+-- Exportação de dados foi desmarcado.
 
 -- Copiando estrutura para tabela db_aula.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -59,14 +63,9 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `senha` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Copiando dados para a tabela db_aula.usuario: ~4 rows (aproximadamente)
-INSERT INTO `usuario` (`id`, `nome`, `telefone`, `email`, `login`, `senha`) VALUES
-	(1, 'Administrador', '49 888008800', 'teste@teste.com', 'admin1', '123'),
-	(2, 'Jackson Five', '84 8989989', 'lordjackson@gmail.com', 'jackson', '123'),
-	(3, 'jackson', '2342342', 'asdas@test.com', 'admin', '$2y$10$DIyFfgrauW5gOHWqIshEsOVDoSoVfE5hV14Te.baQh86BZus5TNkq'),
-	(12, 'Eduardo', '49991767992', 'eduardo.robettibedin@gmail.com', 'dudu', '$2y$10$4HQRH500cvS50Ap6g.IPSe24hVMIcF7BTjFL3WUCZwzQXFkP9TWE2');
+-- Exportação de dados foi desmarcado.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
